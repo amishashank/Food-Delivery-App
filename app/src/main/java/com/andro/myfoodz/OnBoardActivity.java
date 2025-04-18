23e527e41f31a7204e0b2745df7dfc944c43197b
Shashank Pandey <shashankpandey8797@gmail.com>
2025-04-18 13:57:01 +0530

    Initial commit

--- app/src/main/java/com/andro/myfoodz/OnBoardActivity.java
+++ app/src/main/java/com/andro/myfoodz/OnBoardActivity.java
@@ -1,6 +1,10 @@
 package com.andro.myfoodz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class OnBoardActivity extends AppCompatActivity {
-
+Button onboard_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_on_board);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        onboard_button = findViewById(R.id.onboard_button);
+
+        onboard_button.setOnClickListener(v -> {
+            Intent intent = new Intent(OnBoardActivity.this, LoginActivity.class);
+            Toast.makeText(OnBoardActivity.this, "Hello from MainActivity", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
         });
     }
 }
